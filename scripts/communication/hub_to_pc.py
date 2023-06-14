f18a1436c5c9f7fd44b11de5fcf52f046be057e2
HeberTU <heber.trujillo@glovoapp.com>
2023-06-14 16:44:57 +0200

    feat: initialize python script to connect hub to pc.

---
+++ scripts/communication/hub_to_pc.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+
+Created on: 14/6/23
+@author: Heber Trujillo <heber.trj.urt@gmail.com>
+Licence,
+"""
+from pybricks.pupdevices import Motor
+
+from usys import stdin, stdout
+from uselect import poll
+
