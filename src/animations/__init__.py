34e493474592311df7a2a194ea3a6e7578740bc0
HeberTU <heber.trujillo@glovoapp.com>
2023-06-16 22:42:14 +0200

    feat: create faces class.

--- src/animations/__init__.py
+++ src/animations/__init__.py
@@ -5,3 +5,8 @@ Created on: 16/6/23
 @author: Heber Trujillo <heber.trj.urt@gmail.com>
 Licence,
 """
+from src.animations.faces import Face
+
+__all__ = [
+    "Face"
+]
--- src/animations/faces.py
+++ src/animations/faces.py
@@ -5,3 +5,32 @@ Created on: 16/6/23
 @author: Heber Trujillo <heber.trj.urt@gmail.com>
 Licence,
 """
+from pybricks.geometry import Matrix
+
+ON = 100
+OFF = 0
+
+
+class Face:
+    """This classe  is used to represent custom face animations."""
+
+    EYES_OPEN: Matrix = Matrix(
+        [
+            [OFF, OFF, OFF, OFF, OFF],
+            [ON,  ON,  OFF, ON,  ON],
+            [ON,  ON,  OFF, ON,  ON],
+            [ON,  ON, OFF,  ON,  ON],
+            [OFF, OFF, OFF, OFF, OFF],
+        ]
+    )
+
+    EYES_CLOSE: Matrix = Matrix(
+        [
+            [OFF, OFF, OFF, OFF, OFF],
+            [OFF, OFF, OFF, OFF, OFF],
+            [ON,  ON,  OFF, ON,  ON],
+            [OFF, OFF, OFF, OFF, OFF],
+            [OFF, OFF, OFF, OFF, OFF],
+        ]
+    )
+
