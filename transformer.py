68d867f974524a7fec6df7d7114bdda131e3ba7b
HeberTU <heber.trujillo@glovoapp.com>
2023-07-01 17:40:54 +0200

    feat: Refactor transformer walking script.

--- transformer.py
+++ transformer.py
@@ -5,19 +5,24 @@ Created on: 26/6/23
 @author: Heber Trujillo <heber.trj.urt@gmail.com>
 Licence,
 """
-import umath
 from pybricks.hubs import InventorHub
 from pybricks.pupdevices import Motor, UltrasonicSensor
 from pybricks.parameters import Port
 
 from pybricks.tools import wait
 
+from src.movement.walking import (
+    set_legs_to_initial_position,
+    walk,
+    turn_left_by_n_steps
+)
+
 
 def initialize_robot(
         right_leg_port: Port,
         left_leg_port: Port,
 ):
-    """
+    """Initialize robot legs.
 
     Args:
         right_leg_port: Port
@@ -39,206 +44,6 @@ def initialize_robot(
     return legs
 
 
-def get_waiting_time_to_finish_rotation(
-        speed: int,
-        rotation_angle: int
-) -> int:
-    """Get how much time is needed to wait for a motor to rotate the specified
-    degrees.
-
-    Args:
-        speed: int
-            degrees / seconds
-        rotation_angle: int
-            number of degrees to rotate
-
-    Returns:
-        int:
-            Number of milliseconds to wait.
-    """
-    return umath.ceil((rotation_angle / speed) * 1000)
-
-# TODO heber.trujilloglovoapp.com - 1/7/23: Change to run target method.
-def set_legs_to_initial_position(
-        right_leg: Motor,
-        left_leg: Motor,
-        speed: int,
-        initial_angle: int = -90
-) -> None:
-    """Set legs to the initial position to do any movement.
-
-        The initial position is one foot completely forward and the other
-        completely backward.
-
-    Args:
-        right_leg: Motor
-            Right leg motor.
-        left_leg: Motor
-            Left leg motor
-        speed: int
-            degrees / seconds
-        initial_angle:
-            Initial angle to rotate.
-    """
-
-    right_rotation_angle = (initial_angle - right_leg.angle())
-    left_rotation_angle = (initial_angle - left_leg.angle())
-
-    right_leg.run_angle(
-        speed=speed,
-        rotation_angle=right_rotation_angle,
-        wait=False
-    )
-
-    left_leg.run_angle(
-        speed=speed,
-        rotation_angle=left_rotation_angle,
-        wait=False
-    )
-
-    time_to_wait = get_waiting_time_to_finish_rotation(
-        speed=speed,
-        rotation_angle=max(
-            abs(right_rotation_angle),
-            abs(left_rotation_angle)
-        )
-    )
-
-    wait(time_to_wait + 100)
-
-    right_leg.reset_angle()
-    left_leg.reset_angle()
-
-
-def walk(
-        right_leg: Motor,
-        left_leg: Motor,
-        speed: int,
-        forward: bool = True
-) -> None:
-    """
-
-    Args:
-        right_leg: Motor
-            Right leg motor.
-        left_leg: Motor
-            Left leg motor.
-        speed: int
-            degrees / seconds.
-        forward: bool = True
-            If True, the walking movement will be forward.
-    """
-    direction = -1 if forward else 1
-
-    right_leg.run(
-        speed=speed * direction
-    )
-    left_leg.run(
-        speed=speed * -direction
-    )
-
-
-def turn(
-        right_leg: Motor,
-        left_leg: Motor,
-        speed: int,
-        left: bool = True
-) -> None:
-    """Turn into the provideddirection
-
-    Args:
-        right_leg: Motor
-            Right leg motor.
-        left_leg: Motor
-            Left leg motor.
-        speed: int
-            degrees / seconds.
-        left: bool = True
-            If true, the turn will be in the left direction.
-    """
-    rotation_angle = 180
-
-    direction = -1 if left else 1
-
-    wating_to_finish = get_waiting_time_to_finish_rotation(
-        speed=speed,
-        rotation_angle=rotation_angle
-    )
-
-    left_leg.run_angle(
-        speed=speed * -direction,
-        rotation_angle=rotation_angle,
-        wait=False
-    )
-    right_leg.run_angle(
-        speed=speed * -direction,
-        rotation_angle=rotation_angle,
-        wait=False
-    )
-    wait(wating_to_finish + 200)
-
-    # One step forward to balance
-    left_leg.run_angle(
-        speed=speed * -direction,
-        rotation_angle=rotation_angle,
-        wait=False
-    )
-
-    right_leg.run_angle(
-        speed=speed * direction,
-        rotation_angle=rotation_angle,
-        wait=False
-    )
-    wait(wating_to_finish + 200)
-
-
-def turn_left_by_n_steps(
-        right_leg: Motor,
-        left_leg: Motor,
-        speed: int,
-        steps: int
-) -> None:
-    """Turn left to n steps.
-
-    Args:
-        right_leg: Motor
-            Right leg motor.
-        left_leg: Motor
-            Left leg motor.
-        speed: int
-            degrees / seconds.
-        steps: int
-            Number of steps to turn, each step is equivalent to 80 deg turn.
-    """
-    left_leg.stop()
-    right_leg.stop()
-
-    left_leg.reset_angle()
-    right_leg.reset_angle()
-
-    set_legs_to_initial_position(
-        right_leg=right_leg,
-        left_leg=left_leg,
-        speed=300,
-    )
-
-    for i in range(steps):
-        turn(
-            right_leg=right_leg,
-            left_leg=left_leg,
-            speed=speed
-        )
-
-    for leg in [right_leg, left_leg]:
-        leg.reset_angle()
-
-    set_legs_to_initial_position(
-        right_leg=right_leg,
-        left_leg=left_leg,
-        speed=300,
-    )
-
-
 def main():
     """Main script"""
     _ = InventorHub()
@@ -252,7 +57,7 @@ def main():
     set_legs_to_initial_position(
         right_leg=right_leg,
         left_leg=left_leg,
-        speed=300,
+        speed=200,
     )
 
     while True:
@@ -270,7 +75,7 @@ def main():
         turn_left_by_n_steps(
             right_leg=right_leg,
             left_leg=left_leg,
-            speed=300,
+            speed=200,
             steps=5
         )
 
